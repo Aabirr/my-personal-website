260b47d89529da7d3cb55265c9a9586a881f3e8a
Aabir <aabirkashif@gmail.com>
2024-12-30 06:13:07 +0600

    Fixed email link

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const a = 15;
+let b = a;
+
+console.log(b + 10);
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(10, 12));
+
+function minus() {
+  let nothing = "Aabir";
+  return nothing;
+}
+
+console.log(minus());
+
+let car = {
+  toyota: "Vlvo",
+};
+
+console.log(car.toyota);
+
+let dog = {
+  buldog: "Buldog",
+  age: 15,
+  color: "White",
+};
+
+console.log(`The dog is a ${dog.buldog} ${dog.age} old ${dog.color} color dog`);
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(multiply(a, b));
